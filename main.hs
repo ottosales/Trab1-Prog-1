ca41491a963cfbec181ea17b4db23f06c53630f8
otavio <otavio@leanonsystems.com>
2020-11-12 14:33:41 -0300

    other input read and random numbers generated

--- main.hs
+++ main.hs
@@ -1,5 +1,7 @@
-import System.IO  
+import System.Random
+import System.IO
 import Data.Char
+import Data.List as List (sort)
 
 import Data.Text as Text (Text, pack, unpack, splitOn)
 -- Input Reading
@@ -9,8 +11,16 @@ type Class = String
 type DataTuple = ([Coords], Class)
 
 main = do
-    infoList <- readInput
-    
+    dataList <- readInput
+    -- outputFileName <- readOutput
+    percTest <- readPercentual
+    let testSize = calcTestSize (read percTest) (length dataList)
+    seed <- readSeed
+    randomList <- genRandom (read seed) testSize (length dataList)
+    print randomList
+    let xs = sort randomList
+    print xs
+
     putStrLn "all finished!"
 
 readInput :: IO [([Double], String)]
@@ -35,4 +45,41 @@ lineToData line = makeTuple (getDoubles (splitLine line)) (getClass line)
 makeTuple :: a -> b -> (a, b)
 makeTuple x y = (x, y)
 
-    
+
+readOutput :: IO String
+readOutput = do
+    putStrLn "Please inform the output file name:"
+    getLine
+
+readPercentual :: IO String
+readPercentual = do
+    putStrLn "Please inform the examples percentual:"
+    getLine
+
+readSeed :: IO String
+readSeed = do
+    putStrLn "Please inform the seed for the random program:"
+    getLine
+
+tem :: Eq a => a -> [a] -> Bool
+tem _ [] = False
+tem y (x:xs)
+   | x == y = True
+   | otherwise = tem y xs
+
+removeDup :: Eq a => [a] -> [a]
+removeDup l = removeD l []
+   where
+     removeD [] _ = []
+     removeD (x:xs) ls
+        | tem x ls = removeD xs ls
+        | otherwise = x: removeD xs (x:ls)
+
+genRandom seed testSize limit= do 
+    let g = mkStdGen seed
+    let aleatorios = take testSize (removeDup (randomRs (0,limit) g :: [Int]))
+    return aleatorios
+
+
+calcTestSize percTest testSize = (percTest * testSize) `div` 100
+
